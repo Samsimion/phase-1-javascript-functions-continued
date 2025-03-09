b2a18272448969527d8f9d0a38c8115f76da6f7f
simion samuel <samuel.simion@student.moringaschool.com>
2025-03-09 07:32:03 +0300

    cont assign funct

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrap = "*"){
+    return function (adjective){
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+}
+
+let result = wrapAdjective("*");
+let emphatic = result("a hard worker")
+console.log(result + emphatic);
